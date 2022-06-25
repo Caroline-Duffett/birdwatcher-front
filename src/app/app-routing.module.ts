cbe16cbdf38b2412f365c5de18fdd26ab4bedf96
CarolineDuffett <ckduffett@gmail.com>
2022-06-25 15:58:08 -0400

    broke the add form into its own page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core'; //angular, need this
-import { RouterModule, Routes } from '@angular/router'; //imports the RouterModule
+import {NgModule} from '@angular/core'; //angular, need this
+import {RouterModule, Routes} from '@angular/router'; //imports the RouterModule
 import {BirdsIndexComponent} from './birds-index/birds-index.component' //Imports out Birds index because we want to give it a route
 import {BirdShowComponent} from './bird-show/bird-show.component' //Imports out Bird show because we want to give it a route
 import {WelcomePageComponent} from './welcome-page/welcome-page.component' //Imports the welcome page component
+import {AddFormComponent} from "./add-form/add-form.component";
 
 
 //Where we set the routes we want
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: '', redirectTo:'/welcome', pathMatch: 'full'}, //not sure if I need this since it looks like it should redirect with what is already there
   { path: 'birds/:id', component: BirdShowComponent},
   { path: 'birds', component: BirdsIndexComponent},
-  { path: 'welcome', component: WelcomePageComponent }
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'add-bird', component: AddFormComponent}
 ];
 
 
