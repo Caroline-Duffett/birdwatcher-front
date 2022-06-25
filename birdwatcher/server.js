1f9cab2904ce53103eddaa2631c26f46009ef48c
CarolineDuffett <ckduffett@gmail.com>
2022-06-25 13:04:36 -0400

    trying a different listener

--- birdwatcher/server.js
+++ birdwatcher/server.js
@@ -9,7 +9,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname+'/dist/birdwatcher/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
 
 
 //Note to remember what you did last: made a .env file specifying the port, required .gotenv, and added .env to gitignore. Do not know if I even needed to do this but trying anything at this point
@@ -19,3 +19,7 @@ app.listen(process.env.PORT || 8080);
 // app.listen(process.env.PORT || 3000, function(){
 //   console.log("Express server listening on port %d in %s mode", this.address().PORT, app.settings.env);
 // });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
