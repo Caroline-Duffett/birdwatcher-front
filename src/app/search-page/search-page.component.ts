fbe82284fd9650af3ae3b5f037dc0056cae2dd73
CarolineDuffett <ckduffett@gmail.com>
2022-06-27 17:47:58 -0400

    another attempt with this search bar

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -219,7 +219,7 @@ export class SearchPageComponent implements OnInit {
     this.getBirds();
   }
 
-  //Gets the birds from the bird service (where the birds are being called in from database)
+  Gets the birds from the bird service (where the birds are being called in from database)
   getBirds(): void {
     this.birdService.getBirds()
     .subscribe(birds => this.birds = birds);
