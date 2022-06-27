ad6dc8cb4eb46f924a4f93416d8f15ea664f2e60
CarolineDuffett <ckduffett@gmail.com>
2022-06-27 13:39:05 -0400

    added error handling to the add form

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,252 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import {Bird} from '../bird';
-// import {BirdService} from '../bird.service';
-// import {Location} from '@angular/common'; //Interacts with the browser URL
-// //import {FormsModule} from '@angular/forms';
-//
-// @Component({
-//   selector: 'app-add-form',
-//   templateUrl: './add-form.component.html',
-//   styleUrls: ['./add-form.component.css']
-// })
-//
-// export class AddFormComponent implements OnInit {
-//
-//   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
-//
-//   constructor(
-//     private birdService: BirdService,
-//     private locationAngular: Location
-//   ) { }
-//
-//   ngOnInit(): void {
-//
-//   }
-//
-//   name: string = ''
-//   scientificName: string = ''
-//   image: string = ''
-//   location: string = ''
-//   status: string = ''
-//   description: string = ''
-//
-//
-//   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
-//   getBirds(): void {
-//     this.birdService.getBirds()
-//     .subscribe(birds => this.birds = birds);
-//   }
-//
-//   //Back button
-//   backBtn(): void {
-//     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
-//   }
-//
-//   submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
-//     const newBird = {
-//       name: name,
-//       scientificName: scientificName,
-//       image: image,
-//       location: location,
-//       status: status,
-//       description: description
-//     }
-//
-//     this.birdService.addBird(newBird as Bird).subscribe(() => {
-//       this
-//     })
-//
-//
-//
-//     console.log(newBird);
-//
-//
-//     this.locationAngular.back()
-//
-//     this.getBirds()
-//   }
-//
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import {Bird} from '../bird';
-// import {BirdService} from '../bird.service';
-// import {Location} from '@angular/common'; //Interacts with the browser URL
-// import {FormsModule} from '@angular/forms';
-//
-// @Component({
-//   selector: 'app-add-form',
-//   templateUrl: './add-form.component.html',
-//   styleUrls: ['./add-form.component.css']
-// })
-//
-// export class AddFormComponent implements OnInit {
-//
-//   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
-//
-//   constructor(
-//     private birdService: BirdService,
-//     private locationAngular: Location,
-//     private formsModule: FormsModule
-//   ) { }
-//
-//   ngOnInit(): void {
-//
-//   }
-//
-//   name: string = ''
-//   scientificName: string = ''
-//   image: string = ''
-//   location: string = ''
-//   status: string = ''
-//   description: string = ''
-//
-//
-//   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
-//   getBirds(): void {
-//     this.birdService.getBirds()
-//     .subscribe(birds => this.birds = birds);
-//   }
-//
-//   //Back button
-//   backBtn(): void {
-//     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
-//   }
-//
-//   submit(add: any): void {
-//     // const newBird = {
-//     //   name: name,
-//     //   scientificName: scientificName,
-//     //   image: image,
-//     //   location: location,
-//     //   status: status,
-//     //   description: description
-//     // }
-//
-//     this.birdService.addBird(add as Bird).subscribe(() => {
-//       this
-//     })
-//
-//
-//
-//     console.log(add);
-//
-//
-//     //this.locationAngular.back()
-//
-//     this.getBirds()
-//   }
-//
-// }
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import {Bird} from '../bird';
-// import {BirdService} from '../bird.service';
-// import {Location} from '@angular/common'; //Interacts with the browser URL
-// //import {FormsModule} from '@angular/forms';
-//
-// @Component({
-//   selector: 'app-add-form',
-//   templateUrl: './add-form.component.html',
-//   styleUrls: ['./add-form.component.css']
-// })
-//
-// export class AddFormComponent implements OnInit {
-//
-//   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
-//
-//   constructor(
-//     private birdService: BirdService,
-//     private locationAngular: Location
-//   ) { }
-//
-//   ngOnInit(): void {
-//
-//   }
-//
-//   name: string = ''
-//   scientificName: string = ''
-//   image: string = ''
-//   location: string = ''
-//   status: string = ''
-//   description: string = ''
-//
-//
-//   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
-//   getBirds(): void {
-//     this.birdService.getBirds()
-//     .subscribe(birds => this.birds = birds);
-//   }
-//
-//   //Back button
-//   backBtn(): void {
-//     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
-//   }
-//
-//   submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
-//     const newBird = {
-//       name: name,
-//       scientificName: scientificName,
-//       image: image,
-//       location: location,
-//       status: status,
-//       description: description
-//     }
-//
-//     // if (ng-valid.required === ng-valid[required]) {
-//     //   console.log("form is valid");
-//     //
-//     // }
-//
-//
-//     this.birdService.addBird(newBird as Bird).subscribe(() => {
-//       this
-//     })
-//
-//
-//
-//     console.log(newBird);
-//
-//
-//     this.locationAngular.back()
-//
-//     this.getBirds()
-//   }
-//
-// }
-
-
-
-
-
-
-
-
-
-
-
-
 import { Component, OnInit } from '@angular/core';
 import {Bird} from '../bird';
 import {BirdService} from '../bird.service';
@@ -292,29 +43,13 @@ export class AddFormComponent implements OnInit {
     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
   }
 
-  // submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
-  //   const newBird = {
-  //     name: name,
-  //     scientificName: scientificName,
-  //     image: image,
-  //     location: location,
-  //     status: status,
-  //     description: description
-  //   }
-  //
-  //   this.birdService.addBird(newBird as Bird).subscribe(() => {
-  //     this
-  //   })
-  //
-  //
-  //
-  //   console.log(newBird);
-  //
-  //
-  //   //this.locationAngular.back()
-  //
-  //   this.getBirds()
-  // }
+
+  acceptedName = true;
+  acceptedScientificName = true;
+  acceptedImage = true;
+  acceptedLocation = true;
+  acceptedStatus = true;
+  acceptedDescription = true;
 
 
   submit(add: any): void {
@@ -327,45 +62,63 @@ export class AddFormComponent implements OnInit {
       description: add.form.value.description
     }
 
-    // this.birdService.addBird(add as Bird).subscribe(() => {
-    //   this
-    // })
-//
+    //error message toggling (only thing I could get to work) If passes bird will be added
+    if (add.form.controls.name.status === "INVALID") {
+      this.acceptedName = false;
+      this.acceptedScientificName = true;
+      this.acceptedImage = true;
+      this.acceptedLocation = true;
+      this.acceptedStatus = true;
+      this.acceptedDescription = true;
+    } else if (add.form.controls.scientificName.status === "INVALID") {
+      this.acceptedName = true;
+      this.acceptedScientificName = false;
+      this.acceptedImage = true;
+      this.acceptedLocation = true;
+      this.acceptedStatus = true;
+      this.acceptedDescription = true;
+    } else if (add.form.controls.image.status === "INVALID") {
+      this.acceptedName = true;
+      this.acceptedScientificName = true;
+      this.acceptedImage = false;
+      this.acceptedLocation = true;
+      this.acceptedStatus = true;
+      this.acceptedDescription = true;
+    } else if (add.form.controls.location.status === "INVALID") {
+      this.acceptedName = true;
+      this.acceptedScientificName = true;
+      this.acceptedImage = true;
+      this.acceptedLocation = false;
+      this.acceptedStatus = true;
+      this.acceptedDescription = true;
+    } else if (add.form.controls.status.status === "INVALID") {
+      this.acceptedName = true;
+      this.acceptedScientificName = true;
+      this.acceptedImage = true;
+      this.acceptedLocation = true;
+      this.acceptedStatus = false;
+      this.acceptedDescription = true;
+    } else if (add.form.controls.description.status === "INVALID") {
+      this.acceptedName = true;
+      this.acceptedScientificName = true;
+      this.acceptedImage = true;
+      this.acceptedLocation = true;
+      this.acceptedStatus = true;
+      this.acceptedDescription = false;
+    } else {
+      this.acceptedName = true;
+      this.acceptedScientificName = true;
+      this.acceptedImage = true;
+      this.acceptedLocation = true;
+      this.acceptedStatus = true;
+      this.acceptedDescription = true;
 
-  if (add.form.controls.name.status === "INVALID") {
-    console.log("cannot submit");
-    document.getElementById('name-alert').style.display('block')
-    //nameAlert.style.display = 'block';
-  } else if (add.form.controls.scientificName.status === "INVALID") {
-      // res.send("cannot submit");
-  } else if (add.form.controls.image.status === "INVALID") {
-      // res.send("cannot submit");
-  } else if (add.form.controls.location.status === "INVALID") {
-      // res.send("cannot submit");
-  } else if (add.form.controls.status.status === "INVALID") {
-      // res.send("cannot submit");
-  } else if (add.form.controls.description.status === "INVALID") {
-      // res.send("cannot submit");
-  } else {
-    console.log(add);
-    console.log("can submit");
-    this.birdService.addBird(newBird as Bird).subscribe(() => {
-      this
-    })
-    this.locationAngular.back()
-    this.getBirds()
-  }
-
-
-
-
-
-  //console.log(add);
-
-
-    //this.locationAngular.back()
-
-    this.getBirds()
+      this.birdService.addBird(newBird as Bird).subscribe(() => {
+        this
+      })
+      this.locationAngular.back()
+      this.getBirds()
+    }
   }
 
 }
@@ -416,5 +169,78 @@ export class AddFormComponent implements OnInit {
   //   //this.locationAngular.back()
   //
   //   //this.getBirds()
+  // }
+//--------------------------------------------------------------------------------------------------------------------//
+
+
+//-------------------------------------------------------------------------- Original Before Error handling added ---//
+  // import { Component, OnInit } from '@angular/core';
+  // import {Bird} from '../bird';
+  // import {BirdService} from '../bird.service';
+  // import {Location} from '@angular/common'; //Interacts with the browser URL
+  // //import {FormsModule} from '@angular/forms';
+  //
+  // @Component({
+  //   selector: 'app-add-form',
+  //   templateUrl: './add-form.component.html',
+  //   styleUrls: ['./add-form.component.css']
+  // })
+  //
+  // export class AddFormComponent implements OnInit {
+  //
+  //   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
+  //
+  //   constructor(
+  //     private birdService: BirdService,
+  //     private locationAngular: Location
+  //   ) { }
+  //
+  //   ngOnInit(): void {
+  //
+  //   }
+  //
+  //   name: string = ''
+  //   scientificName: string = ''
+  //   image: string = ''
+  //   location: string = ''
+  //   status: string = ''
+  //   description: string = ''
+  //
+  //
+  //   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
+  //   getBirds(): void {
+  //     this.birdService.getBirds()
+  //     .subscribe(birds => this.birds = birds);
+  //   }
+  //
+  //   //Back button
+  //   backBtn(): void {
+  //     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
+  //   }
+  //
+  //   submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
+  //     const newBird = {
+  //       name: name,
+  //       scientificName: scientificName,
+  //       image: image,
+  //       location: location,
+  //       status: status,
+  //       description: description
+  //     }
+  //
+  //     this.birdService.addBird(newBird as Bird).subscribe(() => {
+  //       this
+  //     })
+  //
+  //
+  //
+  //     console.log(newBird);
+  //
+  //
+  //     this.locationAngular.back()
+  //
+  //     this.getBirds()
+  //   }
+  //
   // }
 //--------------------------------------------------------------------------------------------------------------------//
