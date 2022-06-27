f183753267772c89c16fd78d4a17430a67934d8f
CarolineDuffett <ckduffett@gmail.com>
2022-06-27 11:21:36 -0400

    have ngform showing up in the console now, but still not working

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,8 +1,89 @@
+// import { Component, OnInit } from '@angular/core';
+// import {Bird} from '../bird';
+// import {BirdService} from '../bird.service';
+// import {Location} from '@angular/common'; //Interacts with the browser URL
+// //import {FormsModule} from '@angular/forms';
+//
+// @Component({
+//   selector: 'app-add-form',
+//   templateUrl: './add-form.component.html',
+//   styleUrls: ['./add-form.component.css']
+// })
+//
+// export class AddFormComponent implements OnInit {
+//
+//   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
+//
+//   constructor(
+//     private birdService: BirdService,
+//     private locationAngular: Location
+//   ) { }
+//
+//   ngOnInit(): void {
+//
+//   }
+//
+//   name: string = ''
+//   scientificName: string = ''
+//   image: string = ''
+//   location: string = ''
+//   status: string = ''
+//   description: string = ''
+//
+//
+//   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
+//   getBirds(): void {
+//     this.birdService.getBirds()
+//     .subscribe(birds => this.birds = birds);
+//   }
+//
+//   //Back button
+//   backBtn(): void {
+//     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
+//   }
+//
+//   submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
+//     const newBird = {
+//       name: name,
+//       scientificName: scientificName,
+//       image: image,
+//       location: location,
+//       status: status,
+//       description: description
+//     }
+//
+//     this.birdService.addBird(newBird as Bird).subscribe(() => {
+//       this
+//     })
+//
+//
+//
+//     console.log(newBird);
+//
+//
+//     this.locationAngular.back()
+//
+//     this.getBirds()
+//   }
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import {Bird} from '../bird';
 import {BirdService} from '../bird.service';
 import {Location} from '@angular/common'; //Interacts with the browser URL
-//import {FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-form',
@@ -16,7 +97,8 @@ export class AddFormComponent implements OnInit {
 
   constructor(
     private birdService: BirdService,
-    private locationAngular: Location
+    private locationAngular: Location,
+    private formsModule: FormsModule
   ) { }
 
   ngOnInit(): void {
@@ -42,31 +124,119 @@ export class AddFormComponent implements OnInit {
     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
   }
 
-  submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
-    const newBird = {
-      name: name,
-      scientificName: scientificName,
-      image: image,
-      location: location,
-      status: status,
-      description: description
-    }
+  submit(add: any): void {
+    // const newBird = {
+    //   name: name,
+    //   scientificName: scientificName,
+    //   image: image,
+    //   location: location,
+    //   status: status,
+    //   description: description
+    // }
 
-    this.birdService.addBird(newBird as Bird).subscribe(() => {
+    this.birdService.addBird(add as Bird).subscribe(() => {
       this
     })
 
 
 
-    console.log(newBird);
+    console.log(add);
 
 
-    this.locationAngular.back()
+    //this.locationAngular.back()
 
     this.getBirds()
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import {Bird} from '../bird';
+// import {BirdService} from '../bird.service';
+// import {Location} from '@angular/common'; //Interacts with the browser URL
+// //import {FormsModule} from '@angular/forms';
+//
+// @Component({
+//   selector: 'app-add-form',
+//   templateUrl: './add-form.component.html',
+//   styleUrls: ['./add-form.component.css']
+// })
+//
+// export class AddFormComponent implements OnInit {
+//
+//   birds: Bird[] = [] //sets birds property (need this here to get the birds again after this has been added)
+//
+//   constructor(
+//     private birdService: BirdService,
+//     private locationAngular: Location
+//   ) { }
+//
+//   ngOnInit(): void {
+//
+//   }
+//
+//   name: string = ''
+//   scientificName: string = ''
+//   image: string = ''
+//   location: string = ''
+//   status: string = ''
+//   description: string = ''
+//
+//
+//   //Gets the birds from the bird service (where the birds are being called in from database). Need to here to grab the data again after it was added. This function gets called in the submit function.
+//   getBirds(): void {
+//     this.birdService.getBirds()
+//     .subscribe(birds => this.birds = birds);
+//   }
+//
+//   //Back button
+//   backBtn(): void {
+//     this.locationAngular.back() //really just location (had to rename because I made an input field called location)
+//   }
+//
+//   submit(name: string, scientificName: string, image: string, location: string, status: string, description: string): void {
+//     const newBird = {
+//       name: name,
+//       scientificName: scientificName,
+//       image: image,
+//       location: location,
+//       status: status,
+//       description: description
+//     }
+//
+//     this.birdService.addBird(newBird as Bird).subscribe(() => {
+//       this
+//     })
+//
+//
+//
+//     console.log(newBird);
+//
+//
+//     this.locationAngular.back()
+//
+//     this.getBirds()
+//   }
+//
+// }
+
+
+
+
+
+
+
+
 
 
 
