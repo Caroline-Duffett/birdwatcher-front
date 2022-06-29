c16a08d394fb242e514d13ff1007f6838bc2c7d5
CarolineDuffett <ckduffett@gmail.com>
2022-06-29 11:45:20 -0400

    made it so if it is empty no search results return

--- src/app/search-component/search-component.component.ts
+++ src/app/search-component/search-component.component.ts
@@ -1,3 +1,42 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Observable, Subject } from 'rxjs';
+// import {Bird} from '../bird'; //Calls in the bird model/schema we made
+// import {BirdService} from '../bird.service'; //Imports (injects?) the bird service we set up. Service connects us to database? Brings in the data?
+//
+//
+// @Component({
+//   selector: 'app-search-component',
+//   templateUrl: './search-component.component.html',
+//   styleUrls: ['./search-component.component.css']
+// })
+//
+// export class SearchComponentComponent implements OnInit {
+//
+//   birds: Bird[] = []//index way
+//   searchText: string = ''
+//
+//   constructor(
+//     private birdService: BirdService //defines and injects the bird service we made
+//   ) { }
+//
+//
+//   ngOnInit(): void {
+//     this.getBirds();
+//   }
+//
+//
+//   debugging() {
+//     console.log(this.searchText); //can register search text
+//   }
+//
+//   //Gets the birds from the bird service (where the birds are being called in from database)
+//   getBirds(): void {
+//     this.birdService.getBirds()
+//     .subscribe(birds => this.birds = birds);
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import {Bird} from '../bird'; //Calls in the bird model/schema we made
@@ -29,14 +68,14 @@ export class SearchComponentComponent implements OnInit {
     console.log(this.searchText); //can register search text
   }
 
+
   //Gets the birds from the bird service (where the birds are being called in from database)
   getBirds(): void {
     this.birdService.getBirds()
     .subscribe(birds => this.birds = birds);
   }
+
 }
-
-
 
 
 
