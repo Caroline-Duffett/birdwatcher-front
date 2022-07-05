d521eae41ac90836cbbd9221f894cacc8cb51cc5
CarolineDuffett <ckduffett@gmail.com>
2022-07-04 22:58:58 -0400

    trying to form a table

---
+++ src/app/usersbirds.ts
@@ -0,0 +1,10 @@
+export interface UsersBirds {
+  _id: string,
+  user: string,
+  birdName: string,
+  numberOfBirds: number,
+  date: string,
+  location: string,
+  comments: string,
+  img: string
+}
